29a9fe427b9307034b757508a24a5946b54b245e
Kattis91 <katya.svedmark@gmail.com>
2023-10-29 19:53:17 +0000

    Add a validation_error when a user tries to like twice

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,5 +1,6 @@
+from django.db import IntegrityError
 from rest_framework import serializers
-from .models import Like
+from likes.models import Like
 
 
 class LikeSerializer(serializers.ModelSerializer):
@@ -11,4 +12,12 @@ class LikeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Like
-        fields = ['id', 'owner', 'created_at', 'post']
+        fields = ['id', 'created_at', 'owner', 'post']
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
