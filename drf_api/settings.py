adf62196a963444a5dd4d9b8dbfba0fd65a49272
Kattis91 <katya.svedmark@gmail.com>
2023-10-30 18:03:41 +0000

    Set debug to True

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -63,7 +63,8 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+# DEBUG = 'DEV' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-kattis91-drfapi-qwo1rdn1zfi.ws-eu105.gitpod.io',
                  'https://my-django-rest-framework-196a9d11f298.herokuapp.com/',
